2eb8853929900c574bab9c79312ae1b3a4f35f48
Jerry Xu <jerrysxu@gmail.com>
2023-10-14 20:20:30 -0700

    Clean up bin

--- bin/verycd.sh
+++
@@ -1,4 +0,0 @@
-for i in $(curl -s -S http://www.verycd.gdajie.com/topics/$1/ | grep detail | sed 's/.*detail.htm.id=\(.*\)\".*/\1/')
-do
-	open -a aMule $(curl -s -S http://www.verycd.gdajie.com/detail.htm?id=$i | grep ed2k_links |  grep file | sed 's/.*= .\(ed2k.*\/\).;/\1/')
-done
